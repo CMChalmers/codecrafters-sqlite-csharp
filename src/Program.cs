df59129a50a3a64c05ff25caa2074ed8426b2b76
Charlie Chalmers <charlie.chalmersdev@gmail.com>
2022-11-26 00:22:11 +0000

    Print table names

--- src/Program.cs
+++ src/Program.cs
@@ -45,6 +45,33 @@ if (command == ".dbinfo")
     // Uncomment this line to pass the first stage
     Console.WriteLine($"number of tables: {schemas.Length}");
 }
+else if(command == ".tables")
+{
+    // Parse page header from database
+    var pageHeader = PageHeader.Parse(database[100..108]);
+
+    // Obtain all cell pointers
+    var cellPointers = database[108..]
+        .Chunk(2)
+        .Take(pageHeader.NumberOfCells)
+        .Select(bytes => ReadUInt16BigEndian(bytes.Span));
+
+    // Obtain all schema records
+    var schemas = cellPointers
+        .Select(cellPointer =>
+        {
+            var stream = database[cellPointer..];
+
+            var (_payloadSize, bytesRead1) = Varint.Parse(stream);
+            var (_rowId, bytesRead2) = Varint.Parse(stream[bytesRead1..]);
+
+            var record = Record.Parse(stream[(bytesRead1 + bytesRead2)..]);
+            return Schema.Parse(record);
+        })
+        .ToArray();
+
+        Console.WriteLine(string.Join(" ", schemas.Select(x => x.Name).ToList()));
+}
 else
 {
     throw new InvalidOperationException($"Invalid command: {command}");
